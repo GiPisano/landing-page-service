03da9f763d3bee4296adb8372f3a0741a880e49d
GiPisano <pisanogiacomo9@gmail.com>
2024-07-09 16:49:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <app-header></app-header>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap");
+template {
+  font-family: "Inter", sans-serif;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script></script>
+
+<template>
+  <header>
+    <div class="span">
+      <span>span</span>
+    </div>
+    <div>
+      <h1>nav</h1>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  height: 100px;
+  width: 100%;
+  background-color: blue;
+
+  font-weight: 600;
+  .span {
+    height: 30px;
+    width: 100%;
+    background-color: #ec6c5b;
+  }
+}
+</style>
