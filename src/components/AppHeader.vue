75edd79ab24c1fb4c2f060978d0a689ded57d225
GiPisano <pisanogiacomo9@gmail.com>
2024-07-22 16:36:59 +0200

    add header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,25 +3,184 @@
 <template>
   <header>
     <div class="span">
-      <span>span</span>
+      <p>
+        Ottieni il tuo primo corso con uno sconto del 50%. L'offerta è valida
+        solo per i primi 50 studenti. <a href="#">Registrati adesso</a>
+      </p>
     </div>
-    <div>
-      <h1>nav</h1>
+    <div class="nav">
+      <div class="img-logo">
+        <img src="../assets/images/logo.png" alt="Logo" />
+      </div>
+      <div class="container-btn">
+        <button
+          class="btn menu-button"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasRight"
+          aria-controls="offcanvasRight"
+        >
+          <i class="fa-solid fa-bars"></i>
+        </button>
+      </div>
+      <div class="menu-items">
+        <a href="#"><i class="fa-solid fa-house"></i> HOME</a>
+        <a href="#"><i class="fa-solid fa-laptop"></i> CORSI</a>
+        <a href="#"><i class="fa-solid fa-calendar-days"></i> EVENTI</a>
+        <a href="#"><i class="fa-solid fa-phone"></i> CONTATTI</a>
+        <a href="#"><i class="fa-solid fa-pen-to-square"></i> REGISTRATI</a>
+      </div>
+      <div class="icons">
+        <i class="fa-regular fa-circle-user"></i>
+        <i class="fa-regular fa-heart"></i>
+        <i class="fa-solid fa-bag-shopping"></i>
+        <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+    <div
+      class="offcanvas offcanvas-end"
+      tabindex="-1"
+      id="offcanvasRight"
+      aria-labelledby="offcanvasRightLabel"
+    >
+      <div class="offcanvas-header">
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="offcanvas-body">
+        <div>
+          <a href="#"><i class="fa-solid fa-house"></i> HOME</a>
+        </div>
+        <div>
+          <a href="#"><i class="fa-solid fa-laptop"></i> CORSI</a>
+        </div>
+        <div>
+          <a href="#"><i class="fa-solid fa-calendar-days"></i> EVENTI</a>
+        </div>
+        <div>
+          <a href="#"><i class="fa-solid fa-phone"></i> CONTATTI</a>
+        </div>
+        <div>
+          <a href="#"><i class="fa-solid fa-pen-to-square"></i> REGISTRATI</a>
+        </div>
+      </div>
     </div>
   </header>
 </template>
 
 <style lang="scss" scoped>
-header {
-  height: 100px;
-  width: 100%;
-  background-color: blue;
+p {
+  margin-bottom: 0;
+}
 
-  font-weight: 600;
+.img-logo {
+  width: 50%;
+}
+
+header {
   .span {
-    height: 30px;
     width: 100%;
     background-color: #ec6c5b;
+    color: white;
+    text-align: center;
+    a {
+      color: white;
+    }
   }
+
+  img {
+    max-width: 150px;
+  }
+
+  .nav {
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .icons {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 20px;
+    flex-wrap: wrap;
+  }
+
+  .menu-items {
+    display: none;
+  }
+
+  .menu-button {
+    display: inline-block;
+
+    border: 1px solid black;
+  }
+
+  .container-btn {
+    width: 50%;
+    display: flex;
+    justify-content: end;
+  }
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
+
+  .nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  @media (min-width: 768px) {
+    .menu-items {
+      display: flex;
+      align-items: center;
+      gap: 30px;
+    }
+
+    .menu-button {
+      display: none;
+    }
+
+    .offcanvas {
+      display: none;
+    }
+    .img-logo {
+      width: auto;
+    }
+    .container-btn {
+      display: none;
+    }
+    a {
+      color: black;
+    }
+  }
+
+  .img-nav {
+    width: auto;
+  }
+}
+
+.offcanvas {
+  background-color: var(--gray);
+
+  .btn-close {
+    background-color: white;
+  }
+}
+
+.offcanvas-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 20px;
+  gap: 50px;
 }
 </style>
--- src/components/pages/CodeCircus.vue
+++ src/components/pages/CodeCircus.vue
@@ -31,12 +31,25 @@ export default {
       <div class="container">
         <!-- CODE CIRCUS INTRO -->
         <div class="row">
-          <div class="col-md-6">
-            <h1>title</h1>
-            <p>text</p>
-            <input type="text" />
+          <div class="col-md-6 g-4">
+            <h1>CODECIRCUS</h1>
+            <p>
+              Immergiti nel divertimento del codice, dove ogni byte è una gioia!
+            </p>
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="cosa vuoi imparare oggi?"
+              />
+              <span class="input-group-text">
+                <div class="input-icon">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+              </span>
+            </div>
           </div>
-          <div class="col-md-6">
+          <div class="col-md-6 g-4">
             <img src="../../assets/images/billboard-img.png" alt="" />
           </div>
         </div>
@@ -47,10 +60,10 @@ export default {
       <div class="container">
         <div class="row">
           <div class="col-md-3 card text-center" v-for="card in cards">
-            <h3>
+            <h3 class="title">
               {{ card.title }}
             </h3>
-            <p>{{ card.text }}</p>
+            <h6 class="text">{{ card.text }}</h6>
           </div>
         </div>
       </div>
@@ -63,25 +76,66 @@ export default {
   background-image: url("../../assets/images/billboard-bg.png");
   width: 100%;
   object-fit: contain;
+
   .row {
     --bs-gutter-x: 0;
     object-fit: contain;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    h1 {
+      font-size: 40px;
+      font-weight: 200;
+      width: 100%;
+    }
     img {
       width: 100%;
       height: 100%;
     }
+  }
+
+  .input-icon {
+    background-color: var(--green);
+    color: white;
+    padding: 5px 12px;
+    border-radius: 10px;
+  }
+
+  .input-group {
+    max-width: 600px;
+
+    input {
+      background-color: white;
+      border: none;
+      padding: 12px;
+      border-radius: 10px 0 0 10px;
+    }
+  }
+
+  .input-group-text {
+    background-color: white;
+    border-radius: 0 10px 10px 0;
+    border: none;
   }
 }
 
 #circus-cards {
   .row {
     div {
-      padding: 30px;
+      padding: 30px 10px;
       display: flex;
       align-items: center;
       justify-content: center;
       border: 0;
       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      border-radius: 15px;
+      .title {
+        color: var(--red);
+        font-weight: 800;
+      }
+      .text {
+        font-weight: 600;
+      }
     }
   }
 }
@@ -100,7 +154,7 @@ export default {
     position: relative;
     #circus-cards {
       position: absolute;
-      bottom: -50px;
+      bottom: -70px;
       width: 100%;
       .row {
         justify-content: space-between;
